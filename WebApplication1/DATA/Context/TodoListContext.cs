dbdde3e6357a9b43f872dc8b484039649423b3b6
juanCabral-DJ <juandavidcabral71@gmail.com>
2025-11-20 23:49:22 -0700

    Configuracion inicial de base de datos

---
+++ WebApplication1/DATA/Context/TodoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.API.DATA.Context
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> Tareas { get; set; }
+    }
+}
---
+++ WebApplication1/DATA/Domain/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.API.DATA.Domain
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public bool EstaCompletada { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now; 
+    }
+}
